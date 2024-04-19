3a1b44245dda2ddd99ae3e78cd040e2df0a68e1c
xcHuang <paranoid.xc@gmail.com>
2024-04-19 23:50:41 +0800

    mq

--- application/article/rpc/internal/config/config.go
+++ application/article/rpc/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/zeromicro/go-queue/kq"
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/zrpc"
@@ -8,7 +9,9 @@ import (
 
 type Config struct {
 	zrpc.RpcServerConf
-	DataSource string
-	CacheRedis cache.CacheConf
-	BizRedis   redis.RedisConf
+	DataSource            string
+	CacheRedis            cache.CacheConf
+	BizRedis              redis.RedisConf
+	KqConsumerConf        kq.KqConf
+	ArticleKqConsumerConf kq.KqConf
 }
--- application/article/rpc/internal/logic/publishlogic.go
+++ application/article/rpc/internal/logic/publishlogic.go
@@ -1,8 +1,12 @@
 package logic
 
 import (
+	"beyond/application/article/rpc/internal/code"
 	"beyond/application/article/rpc/internal/model"
+	"beyond/application/article/rpc/internal/types"
 	"context"
+	"strconv"
+	"time"
 
 	"beyond/application/article/rpc/internal/svc"
 	"beyond/application/article/rpc/pb"
@@ -25,12 +29,25 @@ func NewPublishLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PublishLo
 }
 
 func (l *PublishLogic) Publish(in *pb.PublishRequest) (*pb.PublishResponse, error) {
+	if in.UserId <= 0 {
+		return nil, code.UserIdInvalid
+	}
+	if len(in.Title) == 0 {
+		return nil, code.ArticleTitleCantEmpty
+	}
+	if len(in.Content) == 0 {
+		return nil, code.ArticleContentCantEmpty
+	}
 	ret, err := l.svcCtx.ArticleModel.Insert(l.ctx, &model.Article{
 		AuthorId:    in.UserId,
 		Title:       in.Title,
 		Content:     in.Content,
 		Description: in.Description,
 		Cover:       in.Cover,
+		Status:      types.ArticleStatusVisible, // 正常逻辑不会这样写，这里为了演示方便
+		PublishTime: time.Now(),
+		CreateTime:  time.Now(),
+		UpdateTime:  time.Now(),
 	})
 	if err != nil {
 		l.Logger.Errorf("Publish Insert req: %v error: %v", in, err)
@@ -41,6 +58,28 @@ func (l *PublishLogic) Publish(in *pb.PublishRequest) (*pb.PublishResponse, erro
 	if err != nil {
 		l.Logger.Errorf("LastInsertId error: %v", err)
 		return nil, err
+	}
+
+	var (
+		articleIdStr   = strconv.FormatInt(articleId, 10)
+		publishTimeKey = articlesKey(in.UserId, types.SortPublishTime)
+		likeNumKey     = articlesKey(in.UserId, types.SortLikeCount)
+	)
+
+	b, _ := l.svcCtx.BizRedis.ExistsCtx(l.ctx, publishTimeKey)
+	if b {
+		_, err = l.svcCtx.BizRedis.ZaddCtx(l.ctx, publishTimeKey, time.Now().Unix(), articleIdStr)
+		if err != nil {
+			logx.Errorf("ZaddCtx req: %v error: %v", in, err)
+		}
+	}
+
+	b, _ = l.svcCtx.BizRedis.ExistsCtx(l.ctx, likeNumKey)
+	if b {
+		_, err = l.svcCtx.BizRedis.ZaddCtx(l.ctx, likeNumKey, 0, articleIdStr)
+		if err != nil {
+			logx.Errorf("ZaddCtx req: %v error: %v", in, err)
+		}
 	}
 
 	return &pb.PublishResponse{ArticleId: articleId}, nil
